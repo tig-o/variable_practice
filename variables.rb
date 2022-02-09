65e2890377ebd9d596c06aa1fced7b9d2afce8fa
Thiago Silveira <thiago1992@yahoo.com>
2022-02-09 13:27:06 -0700

    Add annotations

--- variables.rb
+++ variables.rb
@@ -9,11 +9,18 @@ unread_emails = 521032
 
 # Variables that are assigned to FLoats
 checking_balance = 1000.25
+
+# Checks bitcoin price in current market
 btc_price = 34321.13
+
 percentage_complete = 82.6
 
 # Variables that are assigned to Booleans
 employee_paid = true
+
+# Validates if a current youtube user is subscribed to channel
 user_subscribed = true
 phone_charging = false
+
+# Validates if your MMO character has an item equipped
 character_item_equipped = true
