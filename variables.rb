25c563530c21f8937cae7f2d0cff7a0e0e75179a
Thiago Silveira <thiago1992@yahoo.com>
2022-02-09 13:04:01 -0700

    Add variables assigned to Integer data type

--- variables.rb
+++ variables.rb
@@ -1,3 +1,8 @@
 # Variables that are assigned to Strings
 username = "tigo"
 pw = "turingisthebest"
+
+# Variables that are assigned to Integers
+twitch_followers = 25
+youtube_subs = 12000
+unread_emails = 521032
